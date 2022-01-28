758cbdba7e67f878ed69c2ddb038da751b554162
Yuri <33545766+yuriyur@users.noreply.github.com>
2022-01-28 21:56:13 +0400

    Static files for develop

--- mediasoft/settings.py
+++ mediasoft/settings.py
@@ -124,6 +124,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT='/var/www/mediasoft/static/'
+
+STATICFILES_DIRS = [
+    '/usr/local/lib/python3.9/dist-packages/django/contrib/admin/static',
+    '/usr/local/lib/python3.9/dist-packages/rest_framework/static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- mediasoft/urls.py
+++ mediasoft/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/city/', include('city.urls')),
     path('api/v1/shop/', include('shop.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
