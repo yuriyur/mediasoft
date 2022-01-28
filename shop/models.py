a97b47e0e1ae2ee2e0f3742d73ef72231efef04d
Yuri <d3v4n9@gmail.com>
2022-01-28 19:26:55 +0400

    Positive integer for house numbers

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,6 @@ class Shop(models.Model):
     name = models.CharField(verbose_name='Магазин', max_length=64)
     city = models.ForeignKey(City, verbose_name='Город', on_delete=models.CASCADE)
     street = models.ForeignKey(Street, verbose_name='Улица', on_delete=models.CASCADE)
-    house = models.IntegerField(verbose_name='Номер дома')
+    house = models.PositiveIntegerField(verbose_name='Номер дома')
     time_open = models.TimeField(verbose_name='Время открытия')
     time_close = models.TimeField(verbose_name='Время закрытия')
